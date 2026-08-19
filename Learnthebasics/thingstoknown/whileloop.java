bb8d42911920ac3a566fb0e16ea48cc8e763d57d
Aliya Banu <aliya26205@gmail.com>
2026-08-19 23:52:27 +0530

    Learing...

---
+++ Learnthebasics/thingstoknown/whileloop.java
@@ -0,0 +1,20 @@
+/*Given a digit d (0 to 9), find the sum of the first 50 positive integers (integers > 0) \
+that end with digit d.
+A number ends with digit d if its last digit is d. */
+package Learnthebasics.thingstoknown;
+import java.util.*;
+public class whileloop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int d=sc.nextInt();
+        int n=0;
+        int sum=0;
+        while(n!=51){
+            if(n%10==d){
+                sum=sum+n;
+            }
+            n++;
+        }
+        System.out.println(sum);
+    }
+}
