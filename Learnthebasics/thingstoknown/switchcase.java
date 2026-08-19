bd9c5684dcb4309e84ebdf3c78b0eb0248e1f5fc
Aliya Banu <aliya26205@gmail.com>
2026-08-19 23:17:49 +0530

    Learing...

---
+++ Learnthebasics/thingstoknown/switchcase.java
@@ -0,0 +1,30 @@
+/*Given the integer day denoting the day number, 
+print on the screen which day of the week it is. 
+Week starts from Monday and for values greater than 7 or less than 1, print Invalid.
+Ensure only the 1st letter of the answer is capitalised.*/
+package Learnthebasics.thingstoknown;
+import java.util.*;
+public class switchcase {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         int day=sc.nextInt();
+         switch(day){
+            case 1:System.out.println("Monday");
+                   break;
+            case 2:System.out.println("Tuesday");
+                   break;
+            case 3:System.out.println("Wednesday");
+                   break;
+            case 4:System.out.println("Thrusday");
+                   break;
+            case 5:System.out.println("Friday");
+                   break;
+            case 6:System.out.println("Saturday");
+                   break;
+            case 7:System.out.println("Sunday");
+                   break;
+            default:System.out.println("Invalid");
+         }
+    }
+}  
+
